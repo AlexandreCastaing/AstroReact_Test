7726d4fd44037ca1a1d7f9a8645be469f2ff11d0
Michaela <michell.alfoldiova@gmail.com>
2022-01-25 21:23:14 +0100

    fix(result): class component to function

--- src/components/result.js
+++ src/components/result.js
@@ -2,9 +2,6 @@ import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { WebView } from 'react-native-webview';
 
-// ...
-export default class MyWebComponent extends Component {
-  render() {
-    return <WebView source={{ uri: 'http://192.168.1.95:8888/test3/index2.html' }} incognito={true} />;     
-  }
+export default function MyWebComponent() {
+    return <WebView source={{ uri: 'http://google.com' }} incognito={true} />;
 }
